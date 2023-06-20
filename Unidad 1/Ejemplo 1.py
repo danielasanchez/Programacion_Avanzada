6e2f26679e91a0f24e6613b264a558570b426e94
Daniela Sánchez <danielasanchez.itt@hotmail.com>
2023-06-20 13:35:09 -0700

    second commit

---
+++ Unidad 1/Ejemplo 1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 20 13:33:06 2023
+
+@author: Daniela
+"""
+
+class Perro():
+    nombre="Whisky"
+    raza = "Chihuahua"
+    edad = 11
+    
+perrito1=Perro()
+perrito2=Perro()
+
+print(perrito1.nombre)
+print(perrito2.nombre)
