3f57b70209d2a788ac2b50127eac8dda59a45722
danielasanchez <danielasanchez.itt@hotmail.com>
2023-08-22 23:08:35 -0700

    update

--- Unidad 1/Ejemplo 12.py
+++ Unidad 1/Ejemplo 12.py
@@ -4,19 +4,19 @@
 """
 
 class Perro():
-    def __init__(self,**kwargs):
+    vacunado=False
+    def __init__(self,*args,**kwargs):
         self.nombre=kwargs.get("nombre")
         self.raza=kwargs.get("raza","Criollo")
         self.edad=kwargs.get("edad")
-        self.vacunado=kwargs.get("vacunado",False)
+        self.vacunas=args
         print("Objeto Creado")
     def vacunar(self):
         self.vacunado=True
 
-perrito1=Perro(nombre="Whisky",edad=11)
-print(perrito1.vacunado)
-perrito1.vacunar()
-print(perrito1.vacunado)
+perrito1=Perro("Parvovirus","Rabia",nombre="Whisky",edad=11) 
+print(perrito1.raza)
+print(perrito1.vacunas)
 
 
 
